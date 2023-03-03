fd08ec278acf41af8e99b964cbda2bd6c3a2587d
Suhyun Kim <ideptor@gmail.com>
2023-03-03 21:53:31 +0000

    quiz page

--- app/views/quiz_views.py
+++ app/views/quiz_views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from random import shuffle
 from werkzeug.utils import redirect
 
 from app import db
@@ -7,16 +7,34 @@ from app.forms import AnswerForm, QuestionForm
 from app.models import Question
 from app.views.auth_views import login_required
 from flask import Blueprint, flash, g, render_template, request, url_for
-
+import sys
 
 bp = Blueprint("quiz", __name__, url_prefix="/quiz")
 
+# questions = list(Question.query.order_by(Question.create_date.desc()))
+# shuffle(questions)
+quiz_idx = 0
+# print(quiz_num, question_list)
 
 @bp.route("/<int:question_id>/")
 def show(question_id: int = 1):
-    form = AnswerForm()
-    question = Question.query.get_or_404(question_id)
-    return question.content
-    # return render_template(
-    #     "question/question_detail.html", question=question, form=form
-    # )
+    # form = AnswerForm()
+    # question = Question.query.get_or_404(question_id)
+    # return question.content
+    print(f"quiz.show(): {quiz_idx}", file=sys.stdout)
+
+    quiz_dict = dict()
+    question_list = Question.query.order_by(Question.create_date.desc())
+    for question in question_list:
+        quiz_dict[question.id] = question
+
+    quiz_ids = sorted(list(quiz_dict.keys()))
+    print(f"quiz.show() quiz_ids: {quiz_ids}", file=sys.stdout)
+
+    
+    return render_template(
+        "quiz/quiz.html", 
+        quiz_num=(quiz_idx+1), 
+        question=question_list.get(quiz_ids[quiz_idx])
+    )
+
